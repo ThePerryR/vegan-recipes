4c75f9ce8bdd57de9c14b813e995b2df988ab15a
Perry Ratcliff <perry.ratcliff@integritystl.com>
2020-08-16 19:29:52 -0500

    gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,26 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const glob = require("glob")
+const path = require("path")
+const fs = require("fs")
+
+exports.onPostBuild = () => {
+  // Configure where the functions are kept and where we want to move them.
+  const srcLocation = `${__dirname}/builtFunctions`
+  const outputLocation = `${__dirname}/public/functions`
+
+  // Get all the functions.
+  const modules = glob.sync("*.js", { cwd: srcLocation })
+  modules.forEach(src => {
+    const moduleSrc = path.join(srcLocation, src)
+    const moduleOut = path.join(outputLocation, path.basename(src, path.extname(src)) + ".js")
+
+    // Copy file to new location.
+    fs.copyFile(moduleSrc, moduleOut, (err) => {
+      if (err) {
+        throw err
+      }
+    })
+  })
+}
