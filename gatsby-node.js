a175c18f052220f865d2a305fc10f856d94308a8
Perry Ratcliff <perry.ratcliff@integritystl.com>
2020-08-16 19:59:28 -0500

    build test

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,29 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-
-const glob = require("glob")
-const path = require("path")
-const fs = require("fs")
-
-exports.onPostBuild = () => {
-  // Configure where the functions are kept and where we want to move them.
-  const srcLocation = `${__dirname}/builtFunctions`
-  const outputLocation = `${__dirname}/public/functions`
-
-  console.log('foo')
-
-  // Get all the functions.
-  const modules = glob.sync("*.js", { cwd: srcLocation })
-  modules.forEach(src => {
-    const moduleSrc = path.join(srcLocation, src)
-    const moduleOut = path.join(outputLocation, path.basename(src, path.extname(src)) + ".js")
-
-    // Copy file to new location.
-    fs.copyFile(moduleSrc, moduleOut, (err) => {
-      if (err) {
-        throw err
-      }
-    })
-  })
-}
